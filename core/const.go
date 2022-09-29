848b8d2a6d83fa3bed69c2db9df307c5d64106ec
NUITEQxRB <bellare545@gmail.com>
2022-09-30 00:07:27 +0530

    depslo - Comments for the const.go file

--- core/const.go
+++ core/const.go
@@ -1,9 +1,18 @@
+/* DEPSLO core
+ *
+ * All the constants used in depslo source code are in this file
+ *
+ * HELP NEEDED: LENGTHINCREASEMAP should be bettered altered to map the reality of string elongation in other languages in comparision to the english language.
+ * If there are any resources on research done on this subject, please let me know. You are also welcome to submit a PR.
+ */
 package core
 
 import "math"
 
+// VOWELS slice has all the vowels in English language
 var VOWELS = []rune{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'}
 
+// LETTERS map has all the psuedo localised characters for characters in English language
 var LETTERS = map[rune]rune{
 	'a': 'α',
 	'b': 'ḅ',
@@ -59,6 +68,7 @@ var LETTERS = map[rune]rune{
 	'Z': 'Ż',
 }
 
+// LENGTHINCREASEMAP map has suggested length elongation for psudeo localised strings in other languages compared to English lanugage
 var LENGTHINCREASEMAP = map[int][2]int{
 	10:             [2]int{200, 300},
 	20:             [2]int{180, 200},
