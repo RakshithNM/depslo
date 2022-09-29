fdf170902f5ef27e89a5e43e233ae1e3934e8c7f
NUITEQxRB <bellare545@gmail.com>
2022-09-30 00:17:25 +0530

    depslo - Comments to the HTTP server file

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	core "github.com/RakshithNM/depslo/core"
 )
 
-// gin route handler
+// Gin route handler to PsuedoLocalise passsed in JSON string
 func translate(c *gin.Context) {
 	var inputStrings map[string]string
 
@@ -16,10 +16,11 @@ func translate(c *gin.Context) {
 		return
 	}
 
+	// PsuedoLocalize the JSON passed in to the endpoint, use the depslo core PsuedoLocalize function
 	c.IndentedJSON(http.StatusOK, core.PsuedoLocalize(inputStrings))
 }
 
-// ping to check if server is running
+// Ping to check if server is running
 func ping(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "pong")
 }
